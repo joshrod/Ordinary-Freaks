30e2dc18187587ae2a82ef4820b4721d420ba36a
joshrod <joshrodriguez17@gmail.com>
2018-04-11 19:44:11 -0500

    Switch hero intro from .gif to video to reduce size, add styling for larger devices, add two videos since last update.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
+/* GLOBAL VARIABLES */
 var playButtons = document.querySelectorAll('div.play-button');
 var lightbox = document.querySelector('div.lightbox');
 var cross = document.querySelector('div.cross');
 var video = document.querySelector('#yt-video');
+var chevron = document.querySelector('#chevron-down');
+var form = document.querySelector('.newsletter');
+var activeButton = 'button-active';
+var emailInput = document.querySelector('.news-email');
+var subBtn = document.querySelector('.submit-btn');
+
+/*TOP OF ABOUT SECTION */
+var aboutTop = document.querySelector('#about').getBoundingClientRect().top + window.pageYOffset;
 
 /* VIDEO IDS */
-var latestId = 'A9xNksQOt6M';
-var id1 = 'vNiIGIs2plQ';
-var id2 = 'C5u6mhSoH00';
-var id3 = 'Vbwz6yLyrHo';
-var id4 = 'MIkm5x6d9bw';
+var latestId = 'w_32BFmDjGc';
+var id1 = 'fyLhHZLrEys';
+var id2 = 'A9xNksQOt6M';
+var id3 = 'vNiIGIs2plQ';
+var id4 = 'C5u6mhSoH00';
+var id5 = 'Vbwz6yLyrHo';
+var id6 = 'MIkm5x6d9bw';
 
 /* YOUTUBE API */
 var tag = document.createElement('script');
@@ -18,6 +29,11 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 window.onload = function() {
+
+	/* SMOOOTHSCROLL WHEN CLICKING ON CHEVRON USING PLUGIN */
+	chevron.onclick = function() {
+		scrollIt(about, 500, 'easeOutQuad');
+	};
 
 	/* PLACE AN INSTANCE OF YOUTUBE PLAYER BY DEFAULT*/
 	window.onYouTubeIframeAPIReady = function() {
@@ -57,6 +73,18 @@ window.onload = function() {
 					displayVideo();
 				};
 				break;
+			case 5:
+				playButtons[5].onclick = function() {
+					player.loadVideoById(id5);
+					displayVideo();
+				};
+				break;
+			case 6:
+				playButtons[6].onclick = function() {
+					player.loadVideoById(id6);
+					displayVideo();
+				};
+				break;
 			default:
 				break;
 		}
@@ -67,6 +95,11 @@ window.onload = function() {
 		lightbox.style.display = 'none';
 		player.stopVideo();
 	}
+
+	/* DISPLAY SUBSCRIPTION SUBMIT UPON EMAIL VERIFICATION */
+	emailInput.addEventListener('keyup', function() {
+		emailInput.value != '' && /^([\w-\.]+@([\w-]+\.)+[\w-]{2,12})?$/.test( emailInput.value ) ? subBtn.classList.add(activeButton) : subBtn.classList.remove(activeButton);
+	});
 }
 
 /* YOUTUBE PLAYER */
